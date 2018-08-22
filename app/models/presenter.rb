6521b55e045dcdad84621a06618e73605e57dc92
Stephen Malek <malek.stephen44@gmail.com>
2018-08-22 15:52:14 -0600

    refactor starred repos

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -3,18 +3,11 @@ class Presenter
   def initialize(current_user)
     @token = current_user.token
     @login = current_user.login
+    @service ||= GithubService.new(current_user)
   end
 
   def starred_repos
-    @conn = Faraday.new('https://api.github.com') do |faraday|
-      faraday.headers['Authorization'] = "token #{@token}"
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = @conn.get("/users/#{@login}/starred")
-    raw_repos = JSON.parse(response.body, symbolize_names: true)
-
-    raw_repos.map do |raw_repo|
+    @service.starred_repos.map do |raw_repo|
       Repository.new(raw_repo)
     end
   end
